38f4ed70d17de5f56143522357407821353eeca5
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-07-21 16:43:44 -0500

    Defecto01_ClaseAudio-Directivo-Aplicación principio DRY

--- backend/app/controllers/audio.js
+++ backend/app/controllers/audio.js
@@ -1,71 +1,56 @@
-const mongoose = require('mongoose')
-const model = require('../models/audio')
-const options = {
-    page: 1,
-    limit: 3
+const mongoose = require('mongoose');
+const model = require('../models/audio');
+
+// Función para parsear ID
+const parseId = (id) => mongoose.Types.ObjectId(id);
+
+// Función auxiliar para manejar las respuestas
+const handleResponse = (res, successMessage) => (err, docs) => {
+    if (err) {
+        res.status(err.status || 500).send({ error: err.message || 'Internal Server Error' });
+    } else {
+        res.send(successMessage ? { message: successMessage, items: docs } : { items: docs });
+    }
 };
-const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
-}
+
+// Función auxiliar para manejar la creación de documentos
+const handleCreationResponse = (res) => handleResponse(res, 'Document created successfully');
+
 /**
  * Obtener DATA de USUARIOS
  */
 exports.getData = (req, res) => {
-    model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
+    model.find({}, handleResponse(res));
+};
+
 /**
- * Obtener DATA de USUARIOS
+ * Obtener un solo usuario
  */
 exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    model.findOne({ _id: parseId(req.params.id) }, handleResponse(res));
+};
+
 /**
- * Obtener DATA de USUARIOS
+ * Actualizar datos del audio
  */
 exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-//Guardar datos del audio en la base de datos
-exports.insertData = (req, res) => {
-    const data = req.body
-    model.create(data, (err, docs) => {
-        if (err) {
-            res.status(422.).send({ error: 'Error' })
-        } else {
-            res.send({ data: docs })
-        }
-
-    })
-}
+    const { id } = req.params;
+    const body = req.body;
+    model.updateOne({ _id: parseId(id) }, body, handleResponse(res, 'Audio updated successfully'));
+};
 
 /**
- * Obtener DATA de USUARIOS
+ * Insertar datos del audio en la base de datos
+ */
+exports.insertData = (req, res) => {
+    const data = req.body;
+    model.create(data, handleCreationResponse(res));
+};
+
+/**
+ * Eliminar datos del audio
  */
 exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    const { id } = req.params;
+    model.deleteOne({ _id: parseId(id) }, handleResponse(res, 'Audio deleted successfully'));
+};
--- backend/app/controllers/directivo.js
+++ backend/app/controllers/directivo.js
@@ -1,75 +1,46 @@
-const mongoose = require('mongoose')
-const model = require('../models/directivo')
-const options = {
-    page: 1,
-    limit: 3
+const mongoose = require('mongoose');
+const model = require('../models/directivo');
+
+const parseId = (id) => mongoose.Types.ObjectId(id);
+
+// Función auxiliar para manejar las respuestas
+const handleResponse = (res, successMessage, errorMessage) => (err, docs) => {
+    if (err) {
+        res.status(500).send({ error: errorMessage || 'Internal Server Error' });
+    } else {
+        res.send({ items: docs || successMessage });
+    }
 };
-const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
-}
-/**
- * Obtener DATA de USUARIOS
- */
+
+// Función auxiliar para manejar la creación de documentos
+const handleCreateResponse = (res) => (err, docs) => {
+    if (err) {
+        res.status(422).send({ error: 'Error al crear el documento' });
+    } else {
+        res.send({ data: docs });
+    }
+};
 
 exports.getData = (req, res) => {
-    model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
-/**
- * Obtener DATA de USUARIOS
- */
+    model.find({}, handleResponse(res));
+};
+
 exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-/**
- * Obtener DATA de USUARIOS
- */
+    model.findOne({ _id: parseId(req.params.id) }, handleResponse(res));
+};
 
 exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-/**
- * Insertar DATA de USUARIOS
- */
-exports.insertData = (req, res) => {
-    const data = req.body
-    model.create(data, (err, docs) => {
-        if (err) {
-            res.status(422.).send({ error: 'Error' })
-        } else {
-            res.send({ data: docs })
-        }
+    const { id } = req.params;
+    const body = req.body;
+    model.updateOne({ _id: parseId(id) }, body, handleResponse(res));
+};
 
-    })
-}
-/**
- * Obtener DATA de USUARIOS
- */
+exports.insertData = (req, res) => {
+    const data = req.body;
+    model.create(data, handleCreateResponse(res));
+};
 
 exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    const { id } = req.params;
+    model.deleteOne({ _id: parseId(id) }, handleResponse(res));
+};
