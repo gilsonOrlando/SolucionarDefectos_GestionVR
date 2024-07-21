baef8e9d682b7f0b7151b7804238d98034da59f1
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-07-21 11:18:39 -0500

    Defecto01_ClaseAdministrativo-Aplicacion principio DRY

--- backend/app/controllers/administrativo.js
+++ backend/app/controllers/administrativo.js
@@ -1,80 +1,56 @@
-const mongoose = require('mongoose')
-const model = require('../models/administrativo')
+const mongoose = require('mongoose');
+const model = require('../models/administrativo');
 
-const options = {
-    page: 1,
-    limit: 3
+const parseId = (id) => mongoose.Types.ObjectId(id);
+
+// Función auxiliar para manejar las respuestas
+const handleResponse = (res) => (err, docs) => {
+    if (err) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    } else {
+        res.send({ items: docs });
+    }
 };
 
-const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
-}
 /**
  * Obtener DATA de USUARIOS
  */
-
 exports.getData = (req, res) => {
-    model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
+    model.find({}, handleResponse(res));
+};
 
 /**
  * Obtener DATA de USUARIOS
  */
-
 exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    model.findOne({ _id: parseId(req.params.id) }, handleResponse(res));
+};
 
-//Actualizar datos del administrativo
+// Actualizar datos del administrativo
 exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-
+    const { id } = req.params;
+    const body = req.body;
+    model.updateOne({ _id: parseId(id) }, body, handleResponse(res));
+};
 
 /**
  * Insertar DATA de USUARIOS
  */
 exports.insertData = (req, res) => {
-    const data = req.body
+    const data = req.body;
     model.create(data, (err, docs) => {
         if (err) {
-            res.status(422.).send({ error: 'Error' })
+            res.status(422).send({ error: 'Error' });
         } else {
-            res.send({ data: docs })
+            res.send({ data: docs });
         }
-
-    })
-}
+    });
+};
 
 /**
  * Obtener DATA de USUARIOS
  */
-
 exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    const { id } = req.params;
+    model.deleteOne({ _id: parseId(id) }, handleResponse(res));
+};
