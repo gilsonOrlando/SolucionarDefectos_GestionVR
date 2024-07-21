198ed6681ca0895228f041f7be0477577759a366
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-07-21 16:45:33 -0500

    Defecto01_ClaseDocente-Documento-Aplicación principio DRY

--- backend/app/routes/docente.js
+++ backend/app/routes/docente.js
@@ -1,40 +1,56 @@
-const express = require('express')
-const Auth = require('../middlewares');
-const controller = require('../controllers/docente')
-const router = express.Router()
+const mongoose = require('mongoose');
+const model = require('../models/docente');
+
+const parseId = (id) => mongoose.Types.ObjectId(id);
+
+// Función auxiliar para manejar las respuestas
+const handleResponse = (res) => (err, docs) => {
+    if (err) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    } else {
+        res.send({ items: docs });
+    }
+};
+
 /**
- * Ruta: /user GET
+ * Obtener DATA de USUARIOS
  */
-router.get(
-    `/`,
-    controller.getData
-)
+exports.getData = (req, res) => {
+    model.find({}, handleResponse(res));
+};
+
 /**
- * Ruta: /user GET
+ * Obtener DATA de USUARIOS
  */
-router.get(
-    `/:id`,
-    controller.getSingle
-)
+exports.getSingle = (req, res) => {
+    model.findOne({ _id: parseId(req.params.id) }, handleResponse(res));
+};
+
 /**
- * Ruta: /user GET
+ * Actualizar datos del docente
  */
-router.post(
-    `/`,Auth.verifyToken,
-    controller.insertData
-)
+exports.updateSingle = (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    model.updateOne({ _id: parseId(id) }, body, handleResponse(res));
+};
+
+// Guardar datos del docente en la base de datos
+exports.insertData = (req, res) => {
+    const data = req.body;
+    model.create(data, (err, docs) => {
+        if (err) {
+            res.status(422).send({ error: 'Error' });
+        } else {
+            res.send({ data: docs });
+        }
+    });
+};
+
 /**
- * Ruta: /user GET
+ * Eliminar datos del docente
  */
-router.put(
-    `/:id`,Auth.verifyToken,
-    controller.updateSingle
-)
-/**
- * Ruta: /user GET
- */
-router.delete(
-    `/:id`,Auth.verifyToken,
-    controller.deleteSingle
-)
-module.exports = router
+exports.deleteSingle = (req, res) => {
+    const { id } = req.params;
+    model.deleteOne({ _id: parseId(id) }, handleResponse(res));
+};
